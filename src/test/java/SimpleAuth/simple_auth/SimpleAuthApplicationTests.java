f42c67565f21de26c2a11879d427c6baf3b8d273
Alan Cavalcante <alanlimaemail@gmail.com>
2025-03-22 01:40:54 -0300

    first commit

---
+++ src/test/java/SimpleAuth/simple_auth/SimpleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package SimpleAuth.simple_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
