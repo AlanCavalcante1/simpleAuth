c15953166e89c0152652b806e8c6b2d27e23fb76
Alan Cavalcante <alanlimaemail@gmail.com>
2025-03-28 00:47:43 -0300

    update RoleEntity

---
+++ src/main/java/simple/simple_auth/domain/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package simple.simple_auth.domain.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import simple.simple_auth.domain.entities.RoleEntity;
+import simple.simple_auth.domain.entities.RoleType;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity> getByName(RoleType roleTypeName);
+}
