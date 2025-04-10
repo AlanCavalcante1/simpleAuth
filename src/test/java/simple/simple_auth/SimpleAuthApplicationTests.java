203c65954f3adc044b540dd4f36de48d22f18ffa
Alan Cavalcante <alanlimaemail@gmail.com>
2025-04-10 00:19:24 -0300

    fix problem that contextLoads its null

--- src/test/java/simple/simple_auth/SimpleAuthApplicationTests.java
+++ src/test/java/simple/simple_auth/SimpleAuthApplicationTests.java
@@ -1,13 +1,19 @@
 package simple.simple_auth;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import simple.simple_auth.domain.controllers.AuthController;
 
 @SpringBootTest
 class SimpleAuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired private AuthController authController;
 
+  @Test
+  void contextLoads() {
+    assertThat(authController).isNotNull();
+  }
 }
